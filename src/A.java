58fda7aa0d657976f73e141e13027f89db4c0e89
rajsekarm1 <rajsekarm@gmail.com>
2014-10-17 14:20:08 +0530

    commit and push

---
+++ src/A.java
@@ -0,0 +1,8 @@
+
+public class A {
+	
+	public static void methodOne(){
+		System.out.println("A class MethodOne");
+	}
+
+}
---
+++ src/B.java
@@ -0,0 +1,12 @@
+
+public class B {
+	
+	public static void methodOne(){
+		System.out.println("B class MethodOne");
+	}
+	
+	public void checkingMethod(){
+		super.methodOne();
+	}
+	
+}
